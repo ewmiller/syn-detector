cbff989c69efed6b374fb81326aea709b874d5a7
Ethan Miller <ethan.w.miller@gmail.com>
2016-11-20 22:00:59 -0500

    added some logic

--- detector.py
+++ detector.py
@@ -9,32 +9,62 @@ import sys
 # once finished, look through the array for any IPs that satisfy the suspicion requirements
 # print these to the console
 
-# counter for total packets processed, and a list to hold unique IPs and their 
-packetCount = 0
-ipArray = []
+packet_count = 0
+address_list = []
 arg = sys.argv[1]
+SYN = 0x02
+SYN_ACK = 0x12
+
+class Address:
+  global SYN
+  global SYN_ACK
+  ip_address = 0
+  syn_sent = 0
+  syn_ack_received = 0
+
+  def __init__(self, ip):
+    self.ip_address = ip
+
+  def increment(self, indicator):
+    if indicator == SYN:
+      self.syn_sent+=1
+    elif indicator == SYN_ACK:
+      self.syn_ack_received+=1
 
 # helper method for processPacket
-def countPacket(packet):
-  print "packet counted"
+def countPacket(addr, indicator):
+  found = False
+  for x in address_list:
+    if x.ip_address == addr:
+      found = true
+      x.increment(indicator)
+  if not found:
+    new_addr = Address(addr)
+    new_addr.increment(indicator)
 
 # lambda function passed to scapy.sniff
 def processPacket(packet):
-  global packetCount
-  packetCount+=1
-  print packetCount
+  global packet_count
+  global SYN
+  global SYN_ACK
+
+  packet_count+=1
+  print packet_count
 
   if(packet.haslayer("TCP")):
     # grab flags from the packet
     flags = packet["TCP"].flags
 
     # do a bitwise AND to see which flag is set
+    # if SYN or SYN-ACK, respond accordingly
     SYN = 0x02
     SYNACK = 0x12
     if flags & SYN:
       print "SYN detected. Source: %s, Dest: %s" % (packet["IP"].src, packet["IP"].dst)
-    elif flags & SYNACK:
+      countPacket(packet["IP"].src, SYN)
+    elif flags & SYN_ACK:
       print "SYN-ACK detected. Source: %s, Dest: %s" % (packet["IP"].src, packet["IP"].dst)
+      countPacket(packet["IP"].dst, SYN_ACK)
 
 # importing the whole pcap file takes a while
 # use sniff(offline="...", prn=customFunction) to process them individually as they come in
