224efc8db167f8e47de2cb05b537b8cb055859b2
Ethan Miller <ethan.w.miller@gmail.com>
2016-11-20 21:11:46 -0500

    can now detect SYN and SYN-ACK packets

--- detector.py
+++ detector.py
@@ -9,17 +9,32 @@ import sys
 # once finished, look through the array for any IPs that satisfy the suspicion requirements
 # print these to the console
 
-# counter for total packets processed, and an array to hold unique IPs
+# counter for total packets processed, and a list to hold unique IPs and their 
 packetCount = 0
-ipArray = [[]]
+ipArray = []
 arg = sys.argv[1]
+
+# helper method for processPacket
+def countPacket(packet):
+  print "packet counted"
 
 # lambda function passed to scapy.sniff
 def processPacket(packet):
+  global packetCount
+  packetCount+=1
+  print packetCount
+
   if(packet.haslayer("TCP")):
-    return packet.show()
+    # grab flags from the packet
+    flags = packet["TCP"].flags
 
-
+    # do a bitwise AND to see which flag is set
+    SYN = 0x02
+    SYNACK = 0x12
+    if flags & SYN:
+      print "SYN detected. Source: %s, Dest: %s" % (packet["IP"].src, packet["IP"].dst)
+    elif flags & SYNACK:
+      print "SYN-ACK detected. Source: %s, Dest: %s" % (packet["IP"].src, packet["IP"].dst)
 
 # importing the whole pcap file takes a while
 # use sniff(offline="...", prn=customFunction) to process them individually as they come in
